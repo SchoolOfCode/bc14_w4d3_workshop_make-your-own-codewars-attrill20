998398ec986fdcd4c5f727f046a6df308deb9b26
James Attrill <attrill20@gmail.com>
2023-04-06 19:53:48 +0100

    kata written

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 /* 
-👉 Write your kata here!
+Mr Roberts the mad Maths teacher, has really lost the plot! 
+
+He's got bored of regular maths teaching and now refuses to use any numbers unless they are prime.
+
+Your job is to check all of his textbooks (tests made up of arrays of positive integers only) and remove all the non-prime numbers (return arrays made up only of prime numbers).
+
+*Reminder - a prime number is a number that can be divided only by itself and 1.*
+
+Good luck!
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+function primeTextbooks (num) {
+  //TODO
+}
